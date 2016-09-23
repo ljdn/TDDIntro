9bc282b3eb2922b16dc8b290595887500cfba373
Lejia Duan <lejia.duan@gmail.com>
2016-09-23 14:01:10 -0500

    compute returns two when number is two

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -4,6 +4,10 @@ public class Factorial {
     public Integer compute(int i) {
         if (i == 1) {
             return 1;
-        } else { return -1; }
+        }
+        else if (i == 2) {
+            return 2;
+        }
+        else { return -1; }
     }
 }
