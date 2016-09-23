1fadf9078c3d84a5f68cfdd10033e35ad8d805f1
Lejia Duan <lejia.duan@gmail.com>
2016-09-23 14:00:04 -0500

    compute returns one when number is one

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,8 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        return -1;
+        if (i == 1) {
+            return 1;
+        } else { return -1; }
     }
 }
