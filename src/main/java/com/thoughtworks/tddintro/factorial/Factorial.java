c1bf7b70771986a2bb3b37c00c3bf478f184c85d
Lejia Duan <lejia.duan@gmail.com>
2016-09-23 14:06:36 -0500

    refactor compute

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,15 +2,11 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i == 1 || i == 0) {
+        if (i == 0) {
             return 1;
         }
-        else if (i == 2) {
-            return 2;
+        else {
+            return i * compute(i-1);
         }
-        else if (i == 3) {
-            return 6;
-        }
-        else { return -1; }
     }
 }
