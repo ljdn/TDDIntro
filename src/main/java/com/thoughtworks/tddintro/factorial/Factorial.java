56dd0e2cf6033ed87d372c0a745beebc2b10dff6
Lejia Duan <lejia.duan@gmail.com>
2016-09-23 14:09:30 -0500

    compute throws IllegalArgumentException when number is negative

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,7 +2,10 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if (i == 0) {
+        if (i < 0) {
+            throw new IllegalArgumentException("Negative number");
+        }
+        else if (i == 0) {
             return 1;
         }
         else {
